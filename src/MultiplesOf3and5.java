22bd4f69dcacdf4d03d08bb8cac8ddfb29f7c190
Jordan Schultz <jschultz11@gmail.com>
2015-07-02 13:39:25 -0700

    Finished

--- src/MultiplesOf3and5.java
+++ src/MultiplesOf3and5.java
@@ -1,7 +1,33 @@
-
+//Find the sum of all multiples of 3 or 5 below 1000
 public class MultiplesOf3and5 {
 
 	public static void main(String args[]){
-		System.out.println("Hello");
+
+		int threes = Summultiplesofthree(1000);
+		int fives = Summultiplesoffive(1000);
+		
+		System.out.println("Answer is "+ threes +" "+ fives +" "+ (threes+fives));
+	}
+	
+	public static int Summultiplesofthree(int threshold){
+		int temp = 0;
+		
+		for(int i = 0 ; i < threshold; i++)
+			if(i % 3 == 0){
+				temp+=i;
+				//System.out.println(i);
+			}
+		return temp;
+	}
+	public static int Summultiplesoffive(int threshold){
+		int temp = 0;
+		
+		for(int i = 0 ; i < threshold; i++)
+			if(i % 5 == 0 && i%15 !=0){ // account for one set of 15 multiples
+				temp+=i;
+				//System.out.println(i);
+
+			}
+		return temp;
 	}
 }
